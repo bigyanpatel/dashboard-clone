7e66f511b8125be63f1b722f9b3405efe4f618c0
Bigyan Darshan Patel <bigyanpatel07769@gmail.com>
2024-01-09 04:10:20 +0530

    Feat: Init header comp

--- components/header.tsx
+++ components/header.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { IoSearch } from "react-icons/io5";
+import { FaCaretDown, FaRegCircleQuestion } from "react-icons/fa6";
+import Image from "next/image";
 
 const Header = () => {
   return (
@@ -7,9 +10,42 @@ const Header = () => {
         flex
         justify-center
         items-center
+        gap-4
+        px-8
+        py-3
+        border-b
+        border-[#D9D9D9]
+        shadow
     "
     >
-      Header fkjd;aslfkj kasldjl
+      <div className="flex gap-4 items-center flex-grow">
+        <h5 className="text-[#1A181E] font-medium text-xl">Payouts</h5>
+        <div className="flex items-center gap-[6px] text-[#4D4D4D] text-sm">
+          <FaRegCircleQuestion />
+          <p>How it works</p>
+        </div>
+      </div>
+      <div className="flex items-center gap-2 px-4 py-[9px] bg-[#F2F2F2] text-[#808080] rounded flex-grow">
+        <IoSearch className="text-lg" />
+        <input
+          type="text"
+          placeholder="Search features, tutorials, etc."
+          className="bg-transparent outline-none w-full"
+        />
+      </div>
+      <div className="flex gap-2 justify-end flex-grow">
+        <button className="text-[#4D4D4D] bg-[#E6E6E6] rounded-full w-10 h-10 flex items-center justify-center">
+          <Image
+            src={"/speaker.png"}
+            alt="speaker logo"
+            width={20}
+            height={20}
+          />
+        </button>
+        <button className="text-[#4D4D4D] bg-[#E6E6E6] rounded-full w-10 h-10 flex items-center justify-center">
+          <FaCaretDown className="text-2xl" />
+        </button>
+      </div>
     </header>
   );
 };
