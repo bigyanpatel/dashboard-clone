0b68b2e11a027d503c39fd302e6cb7d2f11c2070
Bigyan Darshan Patel <bigyanpatel07769@gmail.com>
2024-01-08 21:07:23 +0530

    Init: setup

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DahsboardPageLayout = ({ children }: { children: React.ReactNode }) => {
+  return <main>{children}</main>;
+};
+
+export default DahsboardPageLayout;
